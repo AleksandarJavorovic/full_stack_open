fb3710d9f7ce63339e1b933651c41a139007d669
Aleksandar Javorovic <aleksandarjavorovic@gmail.com>
2025-06-18 03:31:36 +0200

    update index.js and add app.js and middleware.js files

--- part4/blog_list/index.js
+++ part4/blog_list/index.js
@@ -1,17 +1,24 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const logger = require('./utils/logger')
+// const express = require('express')
+// const mongoose = require('mongoose')
+// const logger = require('./utils/logger')
+// const config = require('./utils/config')
+// const blogsRouter = require('./controllers/blogs')
+
+// const app = express()
+
+// mongoose.connect(config.MONGODB_URI)
+
+// app.use(express.json())
+
+// app.use('/api/blogs', blogsRouter)
+
+// app.listen(config.PORT, () => {
+//   logger.info(`Server running on port ${config.PORT}`)
+// })
+const app = require('./app') // the actual Express application
 const config = require('./utils/config')
-const blogsRouter = require('./controllers/blogs')
-
-const app = express()
-
-mongoose.connect(config.MONGODB_URI)
-
-app.use(express.json())
-
-app.use('/api/blogs', blogsRouter)
+const logger = require('./utils/logger')
 
 app.listen(config.PORT, () => {
-  logger.info(`Server running on port ${config.PORT}`)
+  logger.info(`Server running on port ${config.PORT}, visit: http://localhost:${config.PORT}/api/blogs`)
 })
