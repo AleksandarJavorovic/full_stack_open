8d5af40154e0cf0c6a34c96459fd0e5837e2e533
Aleksandar Javorovic <aleksandarjavorovic@gmail.com>
2025-06-18 03:08:53 +0200

    add router handler and mongoose model as separate modules

--- part4/blog_list/index.js
+++ part4/blog_list/index.js
@@ -2,36 +2,15 @@ const express = require('express')
 const mongoose = require('mongoose')
 const logger = require('./utils/logger')
 const config = require('./utils/config')
+const blogsRouter = require('./controllers/blogs')
 
 const app = express()
 
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-// Connect to MongoDB using the URI from config
 mongoose.connect(config.MONGODB_URI)
 
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
+app.use('/api/blogs', blogsRouter)
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
